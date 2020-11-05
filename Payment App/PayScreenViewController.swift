d584774ea1375170ac7c26a55a76a3b7eec4ebf6
Thotsana <thosana.mabotsa@umuzi.org>
2020-11-05 18:48:11 +0200

    Done with the design for home page

---
+++ Payment App/PayScreenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PayScreenViewController.swift
+//  Payment App
+//
+//  Created by Thotsana Mabotsa on 2020/11/05.
+//
+
+import UIKit
+
+class PayScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
